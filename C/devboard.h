a6e00c00863c2660746a13188729dade0e2411e5
smiley22 <smileytwentytwo@gmail.com>
2015-11-29 02:30:53 +0100

    added compiled c example program

--- C/devboard.h
+++ C/devboard.h
@@ -1,12 +1,26 @@
 #ifndef _DEVBOARD_H_
 #define _DEVBOARD_H_
 
-#define LED_IOCTL	0x80000000
-#define VID_BASE	0x60000000
+/* memory-mapped hardware registers */
+#define LED_IOCTL			0x80000000
+#define VID_BASE			0x60000000
+#define LCDC_IOCTL			0x8000A000
+#define LCDC_DATA			0x8000A001
 
-#define CLOCK_SPEED 6800000
+/* modeled after Hitachi HD44780 */
+#define LCD_DISP_CLEAR		0x01
+#define LCD_FUNCTION_SET	0x3C
+#define LCD_DISP_CONTROL	0x0F
+#define LCD_ENTRY_MODE		0x06
 
 void set_leds(unsigned char status);
-void sleep(int m);
+void delay();
+void do_led_animation();
+
+void lcd_init();
+void lcd_command(unsigned char cmd);
+void lcd_clear();
+void lcd_write_char(char c);
+void lcd_write_string(char *s);
 
 #endif /* _DEVBOARD_H_ */
--- C/example.c
+++ C/example.c
@@ -1,13 +1,43 @@
 #include "devboard.h"
 
 int main() {
-	unsigned char status = 0xff;
-	while(1) {
-		set_leds(status++);
-//		status = status >> 1;
-//		if(!status)
-//			status = 0x80;
-//		sleep(100);
+	char *s = "Hello from compiled C program!";
+	lcd_init();
+	lcd_write_string(s);
+	do_led_animation();
+	return 0;
+}
+
+void lcd_init() {
+	lcd_command(LCD_DISP_CLEAR);
+	/* 2-line 5x10 dots format display mode. */
+	lcd_command(LCD_FUNCTION_SET);
+	/* turn display on, cursor on, blink-mode on. */
+	lcd_command(LCD_DISP_CONTROL);
+	/* enable cursor increment mode */
+	lcd_command(LCD_ENTRY_MODE);
+}
+
+void lcd_command(unsigned char cmd) {
+	*((unsigned char*)LCDC_IOCTL) = cmd;
+	/* exec command */
+	*((unsigned char*)LCDC_IOCTL) = 0x80;
+	delay();
+}
+
+void lcd_clear() {
+	lcd_command(LCD_DISP_CLEAR);
+}
+
+void lcd_write_char(char c) {
+	*((char*)LCDC_DATA) = c;
+}
+
+void lcd_write_string(char *s) {
+	while(*s) {
+		lcd_write_char(*s++);
+		delay();
+		delay();
 	}
 }
 
@@ -15,8 +45,26 @@ void set_leds(unsigned char status) {
 	*((unsigned char*)LED_IOCTL) = status; 
 }
 
-void sleep(int m) {
+void delay() {
 	int i;
-	for(i = 0; i < 1000000; i++)
+	for(i = 0; i < 10; i++)
 		;
+}
+
+void do_led_animation() {
+	unsigned char mask = 0xff;
+	int i;
+	while(mask) {
+		set_leds(mask);
+		for(i = 0; i < 5; i++)
+			delay();
+		mask = mask >> 1;
+	}
+	mask = 0x01;
+	while(mask) {
+		set_leds(mask);
+		for(i = 0; i < 5; i++)
+			delay();
+		mask = (mask << 1);
+	}
 }
