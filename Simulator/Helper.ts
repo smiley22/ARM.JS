bd2faec7181d8da7f2349acee31679ae0d352913
unknown <C:\Users\abc\AppData\Roaming\The Bat!\Mail>
2015-12-22 00:05:33 +0100

    Exception definitions, Cpsr class. Extend Number with toUint32 method.

---
+++ Simulator/Helper.ts
@@ -0,0 +1,30 @@
+﻿/**
+  * Extends the Number type.
+  */
+interface Number {
+    /**
+     * Returns an unsigned 32-bit number.
+     */
+    toUint32(): number
+}
+
+/**
+  *	Returns an unsigned 32-bit number.
+  *
+  *	Javascript stores all numbers as double values. Using
+  *	bitwise operations is achieved by internally converting back
+  *	and forth between double and integer representations. Also
+  *	all bitwise operations but >>> will return signed numbers.
+  *
+  *	var A = 0x80000000		(Positive value)
+  *	var B = A & 0xFFFFFFFF	(Negative value, not what we want)
+  *
+  * @param {number} x
+  *	value to convert
+  *
+  * @return {number}
+  *	Returns an unsigned 32-bit number.
+  */
+Number.prototype.toUint32 = function () {
+    return this >>> 0;
+}
