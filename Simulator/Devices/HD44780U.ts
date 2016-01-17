0c9796052e72ffe5442da4af9f78fb756c6cc4c9
smiley22 <smileytwentytwo@gmail.com>
2016-01-17 15:52:28 +0100

    HD44780U scaffolding

---
+++ Simulator/Devices/HD44780U.ts
@@ -0,0 +1,89 @@
+﻿module ARM.Simulator.Devices {
+    /**
+     * Simulates an LCD modeled after the Hitachi HD44780U.
+     */
+    export class HD44780U extends Device {
+        /**
+         * A reference to the set of services provided by the virtual machine.
+         */
+        private service: IVmService;
+
+        /**
+         * The memory-region housing the LCD's memory-mapped hardware registers.
+         */
+        private region: Region;
+
+        /**
+         * Initializes a new instance of the HD44780U class.
+         *
+         * @param {number} baseAddress
+         *  The base address in memory from which to offset any memory-mapped hardware registers.
+         */
+        constructor(baseAddress: number) {
+            super(baseAddress);
+        }
+
+        /**
+         * The method that is called when the device is registered with a virtual machine.
+         *
+         * @param {IVmService} service
+         *  A reference to a set of services provided by the virtual machine.
+         * @return {boolean}
+         *  True if device registration was successful; Otherwise false.
+         */
+        OnRegister(service: IVmService): boolean {
+            this.service = service;
+            // FIXME: verify TypeScript '() => {}' context semantics.
+            this.region = new Region(this.baseAddress, 0x100,
+                (a, t) => { return this.Read(a, t); },
+                (a, t, v) => { this.Write(a, t, v); });
+            if (!service.Map(this.region))
+                return false;
+            return true;
+        }
+
+        /**
+         * The method that is called when the device is removed from a virtual machine.
+         *
+         * @remarks
+         *  A device can use this event to unmap it's H/W registers from memory, dispose
+         *  of timeouts etc.
+         */
+        OnUnregister() {
+            if (this.region)
+                this.service.Unmap(this.region);
+            this.region = null;
+        }
+
+        /**
+         * Invoked when one of the LCD's memory-mapped hardware registers is read.
+         *
+         * @param address
+         *  The address that is read, relative to the base address of the registered region.
+         * @param type
+         *  The quantity that is read.
+         * @return {number}
+         *  The read value.
+         */
+        Read(address: number, type: DataType): number {
+            switch (address) {
+            }
+            return 0;
+        }
+
+        /**
+         * Invoked when one of the LCD's memory-mapped hardware registers is written.
+         *
+         * @param address
+         *  The address that is written, relative to the base address of the registered region.
+         * @param type
+         *  The quantity that is written.
+         * @param value
+         *  The value that is written.
+         */
+        Write(address: number, type: DataType, value: number): void {
+            switch (address) {
+            }
+        }
+    }
+}
